b23b9c3305b857bd81ac7db67d7ad51e881c572c
om2123 <omvakhare150@gmail.com>
2023-09-09 18:37:07 +0530

    basic func completed

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,20 +1,12 @@
 import React, { useContext } from 'react'
-import { useState } from "react";
 import Cards from "./Cards";
 import { MyContext } from './Context/MyContext';
 
 export default function Home() {
-    const { setPage, beers, searchResult, setSearchResult, page } = useContext(MyContext);
-    const [search, setSearch] = useState("");
-
-
-
-   
-
+    const { beers } = useContext(MyContext);
 
     return (
         <>
-            
             <ul className="grid  bg-slate-200 grid-cols-4 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4 mb-4     ">
                 {
                     beers.map((beer) => (
@@ -22,12 +14,8 @@ export default function Home() {
                             <Cards beer={beer} />
                         </li>
                     ))
-
                 }
-                
             </ul>
-
-           
         </>
     )
 }
